93118dcc57c39a6c7fba8e3a01f202f2103bb465
JuliaJoven4 <63252638+JuliaJoven4@users.noreply.github.com>
2023-09-27 16:35:22 +0200

    Empezar de cero (eliminar)

--- backend/controllers/clienteController.js
+++
@@ -1,9 +0,0 @@
-/**Se crea la petición */
-const testing = async function (req, res) {
-    res.status(200).send('Hola testing 2');
-}
-
-/**Se exporta el método anterior para que nuestro archivo de rutas pueda utilizarlo */
-module.exports = {
-    testing
-}
